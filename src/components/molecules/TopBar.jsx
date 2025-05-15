fd88284242cc00b7e8d9790352102bd97ff53129
Gisell Orozco <Giseorozco05@gmail.com>
2025-05-14 22:32:50 -0500

    Mejora de responsive

--- src/components/molecules/TopBar.jsx
+++ src/components/molecules/TopBar.jsx
@@ -1,25 +1,41 @@
 import React from "react";
 import TextGray from "../atoms/TextGray";
-import TopOptions from "../atoms/TopOptions";
-import { MapPinLine, Truck, Percent} from "phosphor-react";
 import ClickableLink from "../atoms/ClickableLink";
+import { MapPinLine, Truck, Percent } from "phosphor-react";
 
 function TopBar() {
+  return (
+    <div className="flex flex-col sm:flex-row sm:justify-between items-center gap-2 px-4 sm:px-8 py-2 bg-gray-100">
+      
+      {/* Texto izquierdo - oculto en mobile */}
+      <div className="hidden sm:block">
+        <TextGray />
+      </div>
 
-    return(
-        <div className=" flex justify-between gap-3 px-8 py-2  bg-gray-100">
-            <TextGray></TextGray>
-
-            <div className="flex gap-3">
-                <ClickableLink icon={MapPinLine} text={"Deliver To "} iconSize={14} fontSize="text-xs" highlight={"423659"} onClick={() => console.log("Clicked Deliver To")}></ClickableLink>
-                <span className="text-gray-300 font-light">|</span>
-                <ClickableLink icon={Truck} text={"Track your order"} iconSize={14} fontSize="text-xs" onClick={() => console.log("Track")}></ClickableLink>
-                <span className="text-gray-300 font-light">|</span>
-                <ClickableLink icon={Percent} text={"All Offers"} iconSize={14} fontSize="text-xs" onClick={() => console.log("Offers")}></ClickableLink>
-            </div>
-        </div>
-    )
-
+      {/* Botones Clickables - siempre visibles */}
+      <div className="flex w-full justify-between sm:w-auto sm:gap-3">
+        <ClickableLink
+          icon={MapPinLine}
+          text="Deliver To"
+          highlight="423659"
+          fontSize="text-xs"
+          onClick={() => console.log("Deliver To")}
+        />
+        <ClickableLink
+          icon={Truck}
+          text="Track your order"
+          fontSize="text-xs"
+          onClick={() => console.log("Track")}
+        />
+        <ClickableLink
+          icon={Percent}
+          text="All Offers"
+          fontSize="text-xs"
+          onClick={() => console.log("Offers")}
+        />
+      </div>
+    </div>
+  );
 }
 
-export default TopBar
+export default TopBar;
